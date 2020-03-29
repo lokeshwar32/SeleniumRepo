018a336c99ca062c502b3bc73f2c0f1bf28b9f54
Lokeshwar Rao <s.lokeshwar.rao@accenture.com>
2020-03-29 19:12:53 +0530

    1st commit from bash

---
+++ src/test/java/demo/TestClass.java
@@ -0,0 +1,17 @@
+package demo;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+
+		helloWorld();
+		
+	}
+
+	private static void helloWorld() {
+
+		System.out.println("This is test Hello World! program for Jenkins demo");
+		
+	}
+
+}
