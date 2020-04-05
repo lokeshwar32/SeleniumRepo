1aee48c18a01918d42ac5d410cccabfd269843ca
s.lokeshwar.rao <s.lokeshwar.rao@HDC3-L-90CT1Z2.dir.svc.accenture.com>
2020-04-05 16:41:51 +0530

    changed to testng class

--- src/test/java/demo/Class2.java
+++ src/test/java/demo/Class2.java
@@ -1,8 +1,11 @@
 package demo;
 
+import org.testng.annotations.Test;
+
 public class Class2 {
 
-	public static void main(String[] args) {
+	@Test
+	public void test() {
 		System.out.println("This is test Hello World! program for Jenkins demo");
 	}
 
